6a3bab6d2acc34f2ffa336a2e94acf7108e5843a
devdan <oluochdancun19@gmail.com>
2022-06-17 13:05:28 +0300

    dom

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+const main = document.getElementById("main")
+
+const header = document.createElement("h1");
+const newHeader = document.createElement("h1")
+newHeader.id="victory"
+
+newHeader.innerHTML="Flatiron School is the champion!";
+
+newHeader.classList.add("victory")
